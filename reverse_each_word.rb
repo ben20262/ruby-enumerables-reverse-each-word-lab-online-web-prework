b25ab99ab4d3f8dc0b1304418d1adc193351a2aa
Benjamin Small <ben20262@aim.com>
2019-11-16 22:28:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   puts array
-  brray = array.each{|word| word.reverse}
+  brray = array.each{|word| word.reverse!}
   puts brray
   brray.join(" ")
 end
